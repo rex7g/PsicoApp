ae43ade8d91431daf86ea4ad951de9ddd232b2b1
Renato Dominguez German <RDominguez@ccn.net.do>
2023-12-19 14:08:46 -0400

    cambio de imagenes y se agrego los videos

---
+++ PsicoApp/Controllers/PreciosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PsicoApp.Controllers
+{
+    public class PreciosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
